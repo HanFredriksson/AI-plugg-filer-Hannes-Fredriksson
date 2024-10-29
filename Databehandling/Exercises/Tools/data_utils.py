9c5e00581caf40613bf10858a5bec836e0f84335
Hannes Fredriksson <hanfre@live.se>
2024-10-29 20:38:08 +0100

    Färdig för ikväll med Execise-01 uppgift 1

--- Databehandling/Exercises/Tools/data_utils.py
+++ Databehandling/Exercises/Tools/data_utils.py
@@ -1,9 +1,9 @@
 def plot_missing_values(data):
     import plotly_express as px
+    import pandas as pd
 
     data_null = pd.DataFrame({"NaN count": data.isna().sum()})
     data_null = data_null[data_null["NaN count"] != 0].sort_values(by= "NaN count", ascending=False)
-    print(data_null)
     fig = px.bar(data_null, x = data_null.index, y = "NaN count", labels = {"index": "Columns"})
 
     return fig.show()
