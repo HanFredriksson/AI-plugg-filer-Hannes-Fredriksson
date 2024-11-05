758618c4917ca22b10b695bdf459bc710be32df1
Hannes Fredriksson <hanfre@live.se>
2024-11-05 13:25:48 +0100

    Börjat på exercises 02 och lagt till white spce remover i tools

--- Databehandling/Exercises/Tools/data_utils.py
+++ Databehandling/Exercises/Tools/data_utils.py
@@ -7,4 +7,20 @@ def plot_missing_values(data):
     fig = px.bar(data_null, x = data_null.index, y = "NaN count", labels = {"index": "Columns"})
 
     return fig.show()
+
+
+def whitespace_remover(dataframe):
+
+    # iterating over the columns
+    for i in dataframe.columns:
+
+        # checking datatype of each columns
+        if dataframe[i].dtype == 'object':
+
+            # applying replace function on column and removes all whitespaces
+            dataframe[i] = dataframe[i].str.replace(" ", "")
         
+        else:
+
+            # if condn. is False then it will do nothing.
+            pass
