f5d3297aabdf76091b2581391bcb76df34e86b4d
Hannes Fredriksson <hanfre@live.se>
2025-01-08 15:05:54 +0100

    Changed to jupyter notebook file

--- Statistiska-metoder/Perm-clac.py
+++ Statistiska-metoder/Perm-clac.py
@@ -1,10 +1,11 @@
 import math
+# Assignemnet 4 
 
 teams = ((10, 3), (8, 2), (4, 2), (3, 1))
-count = []
 
-for i in teams:
-    nr = math.comb(i[0], i[1])
-    count.append(nr)
+count = [math.comb(i[0], i[1]) for i in teams]
 
-print(count)
+outcomes = math.prod(count)
+ 
+
+print(outcomes)
