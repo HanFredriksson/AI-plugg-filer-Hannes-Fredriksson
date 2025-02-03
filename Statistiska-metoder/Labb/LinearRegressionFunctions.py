1ae3fce2fb6cd6a4cd00f6f19e26b9d50f63453a
Hannes Fredriksson <hanfre@live.se>
2025-02-03 14:53:05 +0100

    [Lektion och labb] lite anteckningar från föruläsning och jobb med labb kod buggar

--- Statistiska-metoder/Labb/LinearRegressionFunctions.py
+++ Statistiska-metoder/Labb/LinearRegressionFunctions.py
@@ -37,15 +37,6 @@ class LinearRegression:
         self._confi_lvl = 0.975
         self.coefficients = None
 
-    # @property
-    # def param_size(self):
-
-    #     return self.d, self.n
-
-    # @param_size.setter
-    # def set_param_size (self, _):
-    #     self.d = self.X.shape[1] - 1
-    #     self.n = self.Y.shape[0]
 
     @property
     def sample_size(self):
@@ -62,18 +53,17 @@ class LinearRegression:
 
         Sets the size of sampels to varibal n 
         """
-        self.Y= self.data[sample]
+        self.Y = self.data[sample]
         self.Y = np.column_stack([self.Y])
         self.n = len(self.Y)
 
     @property
-    def params(self):
+    def params (self):
 
         return self.X, self.d
 
     @params.setter
-    
-    def params(self, params):
+    def params (self, params):
         """
         arg: list
 
@@ -81,8 +71,8 @@ class LinearRegression:
 
         Sets the number of parameters to varibal d
         """
-        X = self.data[params].to_numpy()
-        self.X = np.column_stack([np.ones(self.Y.shape[0]),X])
+        X = self.data[params]
+        self.X = np.column_stack([np.ones(len(self.Y)), X])
 
         self.d = len(params)
 
@@ -110,15 +100,20 @@ class LinearRegression:
         """
         Fit the linear regression model with Ordinary Least Squares method
         Sets it to varibal coefficients
+
+        returns: array - An array with Beta-0 as [0] and Beta-n [n]
         """
         
         self.coefficients = np.linalg.pinv(self.X.T @ self.X) @ self.X.T @ self.Y 
+        self.coefficients = self.coefficients.reshape(-1, 1)
         
         return 
 
     def RSS (self):
         """
         Sum of squared residuals, RSS or SSE
+
+        returns: int
         """
 
         RSS = np.sum(np.square(self.Y - (self.X @ self.coefficients)))
@@ -129,9 +124,11 @@ class LinearRegression:
     def Syy (self):
         """
         Variance of the dependent  variable Y
+
+        returns: int
         """
         
-        Syy = np.sum(np.square(self.Y)) - (np.square(np.sum(self.Y))/self.n)
+        Syy = self.n*np.sum(np.square(self.Y)) - (np.square(np.sum(self.Y)))/self.n
 
         return Syy
     
@@ -139,8 +136,10 @@ class LinearRegression:
     def Sxx (self):
         """
         Variance of the independent variable X
+
+        returns: int
         """
-        Sxx = np.sum(np.square(self.X)) - (np.square(np.sum(self.X))/self.n)
+        Sxx = self.n*np.sum(np.square(self.X)) - (np.square(np.sum(self.X)))/self.n
 
         return Sxx
     
@@ -148,8 +147,12 @@ class LinearRegression:
     def Sxy (self):
         """
         Covariance between X and Y, cross-product term
+
+        returns: int
         """
-        Sxy = np.sum(self.X * self.Y) - (np.sum(self.X) * np.sum(self.Y))/self.n
+        x_2 = np.column_stack([[i[1:] for i in self.X]])
+
+        Sxy = np.sum(x_2 * self.Y) - (np.sum(x_2) * np.sum(self.Y))/self.n
 
         return Sxy
 
@@ -157,24 +160,30 @@ class LinearRegression:
     def SSR (self):
         """
         The sum of squares due to regression
+
+        returns: int
         """
         SSR = self.RSS() - self.Syy()
 
         return SSR
 
 
-    def t_statistic (self):
+    def cii (self):
         """
-        Gives the t-statistic value
+        Gives the c value, √cii
+
+        retruns: array - with variance/covariance matrix
         """
 
-        t = np.linalg.pinv(self.X.T @ self.X) * self.variance()
+        cii = np.linalg.pinv(self.X.T @ self.X)
 
-        return t
+        return cii
 
     def variance (self):
         """
         Variance for the sample, sigma2
+
+        retruns: int
         """
     
         var = self.RSS() / (self.n - self.d - 1)
@@ -184,6 +193,8 @@ class LinearRegression:
     def sigma (self):
         """
         Residual standard error, Sigma
+
+        retruns: int
         """
             
         
@@ -197,6 +208,8 @@ class LinearRegression:
         """
         Test the significans of the regression of all paramters
         put through the F-distribution.
+
+        returns: int - For P-value
         """
        
         F_statistic = (self.SSR()/self.d)/self.sigma()
@@ -210,6 +223,8 @@ class LinearRegression:
     def R2 (self):
         """
         Coefficient of multiple determination, R2
+
+        returns: int
         """
 
         R2 = self.SSR() / self.Syy()
@@ -218,28 +233,32 @@ class LinearRegression:
 
     def Significance_individual_variables (self, beta):
         """
-        arg: int (0 = β0)
+        arg: int - For beta-0 (0) to beta-n (n)
 
         Significance tests on individual variables
+
+        returns: int - For significance for an induvidul variables.
         """
         
-        param_statistic = self.coefficients[beta] / (self.sigma() * np.sqrt(self.t_statistic()[beta, beta]))
+        param_statistic = self.coefficients[beta] / (self.sigma() * np.sqrt(self.cii()[beta, beta]))
         p_beta= 2*min(stats.t.cdf(param_statistic, self.n - self.d - 1), stats.t.sf(param_statistic, self.n - self.d - 1))
         
         return p_beta
     
-
+# !!! Self.params måste bort. Får anropa Sxy, Sxx, Syy annorlunda.!!!!
     def p_value_pairs_param (self):
         """
         A function that calculates the Pearson number between all pairs of parameters.
         Pearson correlation number (r)
+
+        Returns: Dict - With an index key form columns of data frame and keys with 
+        columns from dataframe with the Pearson nummber
         """
         p_value_pairs = {"Param":[]}
         
 
         for param in self.data.columns:
             self.params = [param]
-            self.X = np.column_stack([[i[1:] for i in self.X]])
             p_value_pairs[param] = []
 
             for sample in self.data.columns:
@@ -252,16 +271,28 @@ class LinearRegression:
 
         return p_value_pairs
     
-    # !!! Inte testa här ifrån !!!
-    def confidence_interval_predictor(self, param):
-        # ˆβi ± t_α/2 \hatσ^2 √cii
-        # where tα/2 is the appropriate point based on the Tn−d−1 distribution and a confidence level α.
+    def confidence_interval_predictor(self):
+        """
+        Gives the confdence intervall between all predictors
 
+        returns: Dict - With Beta-0, Column from dataframe name for beta-1 to beta-n +- value
+        """
+
+
+        alpha = 1 - self._confi_lvl 
         df = self.n - self.d - 1 
-        t = stats.t.ppf(1 - self._confi_lvl/2, df)
+        t = stats.t.ppf(1 - alpha/2, df)
+        confidence_beta ={}
 
-        se_b = self.variance() * (1/self.n)+(np.square(self.t_statistic()))
-        confidence = (self.coefficients[param], t*np.sqrt(se_b))
-       
-        return confidence
-        # print(f"Confidence interval on predictor β0: {confidence[0]:.3f} ± {confidence[1]:.3f} interval: [{[0]- confidence[1]:.3f}, {self.coefficients[0]+ confidence[1]:.3f}]")
+        for param in range(len(self.coefficients)):
+            se_b = np.sqrt(self.variance() * self.cii()[param, param])
+            confidence = (self.coefficients[param, 0], t * se_b)
+            
+            if param == 0:
+                confidence_beta[f"The Intercept, Beta-0"] = [f"{confidence[0]:.4f} +- {confidence[1]:.4f}"]
+            
+            else:
+                confidence_beta[f"The Parametar {self.data.columns[param - 1]}, Beta-{param}"] = [f"{confidence[0]:.4f} +- {confidence[1]:.4f}"]
+
+        return confidence_beta
+        
