1585fbddf72166a0814800de72c6fb3c992cec95
Hannes Fredriksson <hannesfre@gmail.com>
2025-04-18 22:13:49 +0200

    [Labb2] Stor commit, nästan klar med DataWrangler och recomander, begöver bara spara data och modeller från att den blivit kört en gång.

--- Maskininlärning/Labb/Laboration_2_Film_Recommender/App/DataWrangler.py
+++ Maskininlärning/Labb/Laboration_2_Film_Recommender/App/DataWrangler.py
@@ -18,67 +18,185 @@ Sedan bör gener kolumnen göras om till ett set. För att gör filtrering enkla
 - Beräkning/Aggregering av ranking data för att ge ett standardizerat värde på ranking
     * Lägga till alla typer av ranking värde 0.5 till 5.0 och antelet för varje ranking.
     * En stadrizerad värde för medlet på av alla raningar. 
+    * Ger ett vägt medel från skalan som filmerna har blivit betygsatta. formel 
+
+- Beraäkning/Aggregering av tid data. Tänk på att använda tden när filmen släpptes att utgå ifrån för att titta på hur nyligen det var snack om filmen:
+
+    * days_since_release - Days from movie release to mean timestamp
+    * last_rating - How recently the movie was engaged with
+    * engagement_span - Hur långt från första till senaste.
+    * mean_rating_time - Average time when ratings happened
+    * std_rating_time	- Spread/consistency of rating timestamps
+    * total_engagement - Totalt antalet tags och ratings. 
+
+    - *Target*:
+    * Labels: High, medium, low
+    * KMeans för att hitta gränsvärdena. 
 """
 
 
 import pandas as pd
-import numpy as np
 import os 
-from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer
+from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
+from sklearn.cluster import KMeans
+from sklearn.preprocessing import MinMaxScaler
 
 class MovieData():
-    self.movie_data = data
-    self.movie_data_Tfidf = vector
+    
+    def __init__(self):
+        self.movie_profiles = None
+        self.tfidf_matrix = None
 
-    # Kör automatikst när instansierar klassen
+        self.make_movie_profiles()
+    
 
-    def load_compile_data():
+    def make_movie_profiles(self):
         general_path = os.path.abspath(os.path.join(os.path.dirname( __file__ ), "..", "Data")) # Taget från stackoverflow (https://stackoverflow.com/questions/9856683/using-pythons-os-path-how-do-i-go-up-one-directory)
 
-        movie_data = pd.read_csv(general_path + "\\movies.csv")
+        movies_data = pd.read_csv(general_path + "\\movies.csv")
         ratings_data = pd.read_csv(general_path + "\\ratings.csv")
         tags_data = pd.read_csv(general_path + "\\tags.csv")
 
-        movie_data.insert(2, "year",  movie_data["title"].str.extract(r"\((\d{4})\)"))
-        movie_data["title"] = movie_data["title"].str.replace(r"(\s*\(\d{4})\)", "", regex=True)
+        movies_data.insert(2, "year",  movies_data["title"].str.extract(r"\((\d{4})\)"))
+        movies_data["title"] = movies_data["title"].str.replace(r"\s*\(\d{4}\)", "", regex=True)  
 
-        return movie_data
+        ratings_features = self.rating_aggrigator(ratings_data)
+        time_engagment_features = self.time_features(ratings_data, tags_data, movies_data)
+        bow_vector = self.bow_vectorizer(movies_data)
+        
+        
+        feature_list = [ratings_features, time_engagment_features, 
+            bow_vector]
+        
+        for feature in feature_list:
+            movies_data = movies_data.merge(
+            feature,
+            on="movieId",
+            how="left"
+        )
+        
+        tags_data.dropna(inplace=True)
+        valid_movie_ids = tags_data["movieId"].unique()
+        movies_data = movies_data[movies_data["movieId"].isin(valid_movie_ids)].copy()
+        movies_data.dropna(inplace=True)
+        movies_data.reset_index(drop=True, inplace=True)
+
+        self.tfidf_matrix = self.tfidf_vectorizer(tags_data, movies_data)
+        self.movie_profiles = movies_data
+
     
 
-    def compiler(self):
+    def tfidf_input(self, tags_data, movies_data):
         tags_data["tag"] = tags_data["tag"].astype(str) 
-        tags = tags_data.groupby(["movieId"], as_index=False).aggregate({"tag" : " ".join})
-        tags["tag"] = tags["tag"].str.replace(" ", "|")
+        tfidf_input = tags_data.groupby(["movieId"], as_index=False).aggregate({"tag" : " ".join})
+        tfidf_input["tag"] = tfidf_input["tag"].str.replace(" ", "|")
 
-        movies_data = movies_data.merge(tags, on="movieId", how="left")
-        movies_data["tag"] = movies_data["tag"].fillna("")
-        movies_data["tf_idf_input"] = movies_data["genres"] + " " + movies_data["tag"]
+        tfidf_input = tfidf_input.merge(
+            movies_data[["movieId", "genres"]],
+            on="movieId",
+            how="left")
+        
+        tfidf_input["tag"] = tfidf_input["tag"].fillna("")    
+        tfidf_input["tf_idf_input"] = tfidf_input["genres"] + " " + tfidf_input["tag"]
+        tfidf_input = tfidf_input.drop(["tag", "genres"], axis=1)
+        tfidf_input.dropna(inplace=True)
+        
+        # Align the matrix with the moive profiles
+        tfidf_input = tfidf_input[tfidf_input["movieId"].isin(movies_data["movieId"])]
+        tfidf_input = tfidf_input.set_index("movieId").loc[movies_data["movieId"]].reset_index()
+    
+        return tfidf_input
+    
 
-        movies_data = movies_data.drop("tag", axis=1)
-        pass
+    def tfidf_vectorizer(self, tags_data, movies_data):
+        tfidf_input = self.tfidf_input(tags_data, movies_data)
+        
+        vectorizer = TfidfVectorizer()
+        tfidf_matrix = vectorizer.fit_transform(tfidf_input["tf_idf_input"])
+        
+        return tfidf_matrix
 
-
-    def tfidf_vectorizer(self, tags_gener):
-        pass
 
 
     def rating_aggrigator(self, rating_data):
-        pass
+        feature_list = [0.5, 1.0, 1.5, 2.0, 2.5, 3.0, 3.5, 4.0, 4.5, 5.0]
+        rating_count = rating_data.groupby("movieId")["rating"].value_counts()
+        rating_count = rating_count.rename("count")
+        rating_count = rating_count.to_frame()
+        rating_count.reset_index(level=["rating"], inplace=True)
 
-    def time_aggrigator(self, rating_data, tag_data):
-        # def movie_timestamp(self): if this code gets to big 
-        # Need to know the timestamp in seconds since midnight Coordinated Universal Time (UTC) of January 1, 1970 the movie relesed
-        # Droppa filmer med minder än två posts 
-        pass
+        rating_pivot = rating_count.pivot(columns="rating", values="count")
+        rating_pivot.reset_index(inplace=True)
+        rating_pivot = rating_pivot.fillna(0)
+        
+        existing_cols = [col for col in feature_list if col in rating_pivot.columns]
+        weighted_sum = sum(float(r) * rating_pivot[r] for r in existing_cols)
+        total_ratings = rating_pivot[existing_cols].sum(axis=1)
+        
+        rating_pivot["mean_rating"] = weighted_sum / total_ratings
+        rating_pivot["total_ratings"] = total_ratings
+        rating_pivot.drop(feature_list, axis=1, inplace=True)
+        
+        return rating_pivot
 
+
+    def time_features(self, rating_data, tag_data, movies_data):
+        
+        time_tags = tag_data.drop(["userId", "tag"],axis=1)
+        time_rating = rating_data.drop(["userId", "rating"], axis=1)
+        time_data = pd.concat([time_tags, time_rating])
+
+        feature_list = ["mean_time", "std_time", "last_engagement", "total_engagement", "release_year_unix", "since_release_to_mean", "engagement_span"]
+        time_data_features = time_data.groupby("movieId")["timestamp"].agg(["mean", "std", "min", "max", "count"]).reset_index()
+        time_data_features.columns = ["movieId", "mean_time", "std_time", "min_time", "last_engagement", "total_engagement"]
+        
+        time_data_features["release_date"] = pd.to_datetime(movies_data["year"], format="%Y")
+        time_data_features["release_year_unix"] = (time_data_features["release_date"] - pd.Timestamp("1970-01-01")) // pd.Timedelta("1s")
+        time_data_features["since_release_to_mean"] = time_data_features["mean_time"] - time_data_features["release_year_unix"]
+        time_data_features["engagement_span"] = time_data_features["last_engagement"] - time_data_features["min_time"]
+        time_data_features.drop(["release_date", "min_time"], axis=1, inplace=True)
+
+        scaler = MinMaxScaler()
+        time_data_features[feature_list] = scaler.fit_transform(time_data_features[feature_list])
+        time_data_features.dropna(inplace=True)
+
+        time_data_features = self.engagement_classifier(time_data_features, feature_list)
+        
+
+        return time_data_features
+        
     
-    def bow_vectorizer(self):
+    def bow_vectorizer(self, movies_data):
         genres = ["Action", "Adventure", "Animation", "Children", "Comedy", 
           "Crime", "Documentary", "Drama", "Fantasy", "Film-Noir", 
           "Horror", "Musical", "Mystery", "Romance", "Sci-Fi", "Thriller", 
-          "War", "Western", "(no genres listed)"]
+          "War", "Western"]
         
         vectorizer = CountVectorizer()
         vectorizer.fit(genres)
-        df["BoW_input"] = df["genres"].apply( lambda row: vectorizer.transform([row]).toarray())
-        pass
+       
+        genre_matrix = vectorizer.transform(movies_data["genres"]).toarray()
+        bow_input = pd.DataFrame(genre_matrix, columns=vectorizer.get_feature_names_out())
+        bow_input["movieId"] = movies_data["movieId"].values
+
+        return bow_input
+
+
+    def engagement_classifier (self, time_data_features, feature_list):
+       
+        engagement_cluster = KMeans(n_clusters=5)
+        engagement_cluster.fit(time_data_features[feature_list])
+
+        time_data_features["clusters"] =  engagement_cluster.labels_
+
+        cluster_means = time_data_features.groupby("clusters")[feature_list].mean()
+        sorted_clusters = cluster_means.sort_values(by="std_time").index
+
+        labels = ["Very Low", "Low", "Medium", "High", "Very High"]
+        class_labels = {cluster : labels[i] for i, cluster in enumerate(sorted_clusters)}
+
+        time_data_features["engagement_level"] = time_data_features["clusters"].map(class_labels)
+        time_data_features.drop("clusters", axis=1, inplace=True)
+
+        return time_data_features
+        
--- Maskininlärning/Labb/Laboration_2_Film_Recommender/App/Recomender.py
+++ Maskininlärning/Labb/Laboration_2_Film_Recommender/App/Recomender.py
@@ -1,1 +1,89 @@
+from sklearn.metrics.pairwise import cosine_similarity
+from sklearn.ensemble import RandomForestClassifier
+import pandas as pd
+from DataWrangler import MovieData
 
+
+class Recomander():
+    
+    def __init__(self):
+        
+        get_profiles_matrix = MovieData()
+
+        self.recomandations = None
+        self.movie_profiles = get_profiles_matrix.movie_profiles
+        self.tf_idf_matrix = get_profiles_matrix.tfidf_matrix
+        self.input_movie = None
+
+
+    def find_input_movie(self, input_movie):
+            # Find the movie profile based on title
+            movie_profile = self.movie_profiles[self.movie_profiles["title"] == input_movie]
+            self.input_movie = movie_profile
+
+
+    def candidates_model(self):  
+        bow_columns = self.movie_profiles.columns[15:]
+        input_vector = self.input_movie[bow_columns].values
+        similar_movies = self.movie_profiles[self.movie_profiles["movieId"] != self.input_movie["movieId"].values[0]]
+
+        sims = cosine_similarity(input_vector, similar_movies[bow_columns].values)[0]
+        similar_movies = similar_movies.copy()
+        similar_movies["similarity"] = sims        
+        
+        candidates = similar_movies.sort_values("similarity", ascending=False).head(60)  
+        
+        return candidates
+
+    
+    def scoring(self):
+        candidates = self.candidates_model()
+        
+        input_tfidf = self.tf_idf_matrix[self.input_movie.index[0]]
+        candidates_tfidf = self.tf_idf_matrix[candidates.index]
+
+        sims = cosine_similarity(input_tfidf, candidates_tfidf)[0]
+        candidates = candidates.copy()
+        candidates["similarity"] = sims
+
+        scoring_set = candidates.sort_values("similarity", ascending=False).head(20)
+        
+        return scoring_set
+        
+
+    def pred_engagement_level(self):        
+            feature_list = ["mean_time", "std_time", "last_engagement", 
+                        "total_engagement", "release_year_unix", 
+                        "since_release_to_mean", "engagement_span"]
+            
+            X_train, y_train = self.movie_profiles[feature_list], self.movie_profiles["engagement_level"]
+            random_forest = RandomForestClassifier(n_estimators=10)
+            random_forest.fit(X_train, y_train)
+
+            return random_forest, feature_list
+
+
+    def recommander(self):
+        scoring_set= self.scoring()
+        model, feature_list = self.pred_engagement_level()
+
+        pred = model.predict(scoring_set[feature_list])
+
+        scoring_set["pred_engagement_level"] = pred
+        classes = ['Very High', 'High', 'Medium', 'Low', 'Very Low']
+
+        scoring_set["pred_engagement_level"] = scoring_set["pred_engagement_level"].astype('category')
+
+
+        order = list(set(scoring_set["pred_engagement_level"]) - set(classes)) + classes
+        scoring_set["pred_engagement_level"] = scoring_set["pred_engagement_level"].cat.reorder_categories(order)
+
+        high_engamnate = scoring_set.sort_values(by="pred_engagement_level", ascending=True).head(3)
+        scoring_set.drop(high_engamnate.index, inplace=True)
+
+        recent_realesed = scoring_set.sort_values(by="year", ascending=False).head(2)
+        recommendations = pd.concat([high_engamnate, recent_realesed])
+        recommendations.reset_index(drop=True, inplace=True)
+
+        return recommendations
+        
